919cefdad8f7fbd6c3f42361d1b4d0c6fa1c9f66
Megh Patel <patelmeghmahesh2701@gmail.com>
2026-06-06 10:46:21 +0530

    fix: remove duplicate Bootstrap CSS import in index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
